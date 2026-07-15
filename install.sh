a590a76bf56e3c638a697a23ed9fd3218b91051d
teruyamato0731 <77840326+teruyamato0731@users.noreply.github.com>
2026-07-15 22:09:06 +0900

    fix: 削除したusageを削除

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ set -euo pipefail
 #   - sudo
 #   - apt-get
 # Usage:
-#   ./install.sh or DOTFILES_DIR=/path/to/dotfiles ./install.sh
+#   ./install.sh
 # After running the script, please execute the following command:
 #   git config --global include.path '~/.gitconfig.custom'
 
