d6af3fb86432d9f9e3ed563a1ea51ea7de35a065
teruyamato0731 <77840326+teruyamato0731@users.noreply.github.com>
2026-07-24 16:17:07 +0900

    feat(zsh): improve completion with fzf-tab

--- .config/shell/fzf.sh
+++ .config/shell/fzf.sh
@@ -21,9 +21,6 @@ _fzf_compgen_dir() {
   fi
 }
 
-# Advanced customization of fzf options via _fzf_comprun function
-# - The first argument to the function is the name of the command.
-# - You should make sure to pass the rest of the arguments ($@) to fzf.
 _fzf_comprun() {
   local command="$1"
   shift
